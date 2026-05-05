7fd9a5d1d0a72f5d14ba91d01dbe925ee8d4a26e
Mortiarion <andre-endju@ukr.net>
2026-05-05 12:25:58 +0300

    first commit

---
+++ fe/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+</script>
+
+<header class='container mx-auto px-5'>
+	<nav class='flex gap-5 py-5'>
+		<a href="/">Головна</a>
+		<a href="/about">Про нас</a>
+		<a href="/contact">Контакти</a>
+		<a href="/merchandise">Товари</a>
+
+	</nav>
+</header>
+
+<main class='flex gap-5 flex-col'>
+	<section class='container mx-auto px-5'>
+		<h1>Landing Page</h1>
+	</section>
+
+	<section class='container mx-auto px-5'>
+		<h2>
+			Тут тре описати що ми вирішуєм проблему покупця, які проблеми він має і як наш продукт допомагає їх вирішити. Можна також додати якісь приклади використання або посилання на документацію. Або втілення мрії покупця, як він буде використовувати наш продукт і які результати отримає.
+		</h2>
+	</section>
+
+
+
+	<section class='container mx-auto px-5'>
+		<h2>
+			Тут коротенько тре написати про те, що це за сайт, які його можливості і для кого він буде корисним. Можна також додати якісь приклади використання або посилання на документацію.
+		</h2>
+	</section>
+</main>
+
+<footer></footer>
