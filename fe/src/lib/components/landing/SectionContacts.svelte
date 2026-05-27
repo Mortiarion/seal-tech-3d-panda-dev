1820870d5809fa3b7d6621c4a50b40ecaa5ed2df
Mortiarion <andre-endju@ukr.net>
2026-05-27 09:16:51 +0300

    1. причесав секцію контакти

--- fe/src/lib/components/landing/SectionContacts.svelte
+++ fe/src/lib/components/landing/SectionContacts.svelte
@@ -6,40 +6,52 @@
 					Контакти
 				</p>
 
-                <div class="contact-title">
+                <h3 class="title">
 					Поговоримо про <br />
 					
 					<span>
 						твій проєкт
 					</span>
-				</div>
+				</h3>
 
-                <p class='contact-subtitle'>
+                <p class='subtitle'>
                     Розкажи нам про свої ідеї, а ми допоможемо втілити їх у життя. 
 					Ми відкриті до нових проєктів та співпраці!
                 </p>
 
-                <div class="contact-info">
-                    <div class="contact-item">
-                        <div class="contact-dot"></div>
+                <div class="info">
+                    <div class="item">
+                        <div class="dot"></div>
 
-						<a href="tell:+380668038198" target="_blank">
+						<a 
+							href="tell:+380668038198" 
+							target="_blank"
+							title='Поговоримо?'
+						>
 							+380 66 803 8198
 						</a>
                     </div>
 					
-                    <div class="contact-item">
-                        <div class="contact-dot"></div>
+                    <div class="item">
+                        <div class="dot"></div>
 
-                        <a href="https://t.me/SealTech3D" target="_blank">
+                        <a 
+							href="https://t.me/SealTech3D" 
+							target="_blank"
+							title='Нашишеш нам?'
+						>
                             @SealTech3D в Telegram
                         </a>
                     </div>
 
-                    <div class="contact-item">
-                        <div class="contact-dot"></div>
+                    <div class="item">
+                        <div class="dot"></div>
 
-                        <a href="mailto:sealtech3d@gmail.com" target="_blank">
+                        <a 
+							href="mailto:sealtech3d@gmail.com" 
+							target="_blank"
+							title='Напишеш на пошту?'
+						>
                             sealtech3d@gmail.com
                         </a>
                     </div>
@@ -56,6 +68,7 @@
 					minlength="4"
 					maxlength="8"
 					size="10"
+					autocomplete='on' // тут якась байда тре буде розібратись як це говно працює ато браузер ругається
 				/>
 
                 <input 
@@ -67,6 +80,7 @@
 					minlength="4"
 					maxlength="8"
 					size="10"
+					autocomplete='on' // тут якась байда тре буде розібратись як це говно працює ато браузер ругається
 				/>
 
                 <textarea 
@@ -91,77 +105,78 @@
 		display: grid;
 		grid-template-columns: 1fr 1fr;
 		gap: 3rem;
-	}
 
-	.contact-title {
-		font-family: var(--font-display);
-		font-size: 3rem;
-		margin-bottom: 1rem;
-
-		span {
-			color: var(--accent-green);
-		}
-	}
-
-	.contact-subtitle {
-		color: var(--text2);
-		font-size: 1rem;
-		max-width: 320px;
-		line-height: 1.5;
-	}
-
-	.contact-info {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-		margin-top: 2rem;
-	}
-
-	.contact-item {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-	}
+		.title {
+			font-family: var(--font-display);
+			font-size: 3rem;
+			margin-bottom: 1rem;
 	
-	.contact-dot {
-		width: 6px;
-		height: 6px;
-		background: var(--accent-orange2);
-	}
+			span {
+				color: var(--accent-green);
+			}
+		}
+	
+		.subtitle {
+			color: var(--text2);
+			font-size: 1rem;
+			max-width: 320px;
+			line-height: 1.5;
+		}
+	
+		.info {
+			margin-top: 2rem;
 
-	.contact-form {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-	}
+			.item {
+				display: flex;
+				align-items: center;
+				gap: 1rem;
+			}
 
-	.form-input {
-		background: var(--bg2);
-		border: 1px solid var(--border);
-		color: var(--gray);
-		padding: 12px 14px;
-		font-size: 14px;
-		transition: border-color 1.3s, box-shadow 1.3s, scale 1.3s;
-		outline: none;
-		box-shadow: none;
+			.dot {
+				width: 6px;
+				height: 6px;
+				background: var(--accent-orange2);
+			}
+		}
+	
+		form {
+			.form-input {
+				background: var(--bg2);
+				border: 1px solid var(--border);
+				color: var(--gray);
+				padding: 12px 14px;
+				font-size: 14px;
+				transition: border-color 1.3s, box-shadow 1.3s, scale 1.3s;
+				outline: none;
+				box-shadow: none;
+				
+				&:active {
+					border-color: var(--accent-green);
+					box-shadow: var(--accent-green) 0px 0px 30px, var(--accent-orange) 0px 0px 30px;
+					scale: 1.02;
+				}
+				
+				&:focus {
+					box-shadow: var(--accent-green) 0px 0px 30px, var(--accent-orange) 0px 0px 30px;
+				}
 		
-		&:active {
-			border-color: var(--accent-green);
-			box-shadow: var(--accent-green) 0px 0px 30px, var(--accent-orange) 0px 0px 30px;
-			scale: 1.02;
-		}
+				&::placeholder {
+					color: var(--gray);
+				}
+			}
 		
-		&:focus {
-			box-shadow: var(--accent-green) 0px 0px 30px, var(--accent-orange) 0px 0px 30px;
+			textarea.form-input {
+				height: 100px;
+				resize: none;
+			}
 		}
 
-		&::placeholder {
-			color: var(--gray);
+		.info,
+		form {
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
 		}
-	}
-
-	textarea.form-input {
-		height: 100px;
-		resize: none;
+	
 	}
 </style>
